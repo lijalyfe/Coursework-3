13a3fd277eebe9f7de26bf76dd42471613326261
lija_lyfe <lija6556@list.ru>
2023-06-02 18:14:06 +0500

    write def get_filtered_data

--- utils/utils.py
+++ utils/utils.py
@@ -7,3 +7,10 @@ def get_data():
     with open('operations.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
     return data
+
+
+def get_filtered_data(data, filter_empty_from=False):
+    data = [d for d in data if 'state' in d and d['state'] == 'EXECUTED']
+    if filter_empty_from:
+        data = [d for d in data if 'from' in d]
+    return data
