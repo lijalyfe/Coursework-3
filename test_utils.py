dc39d04844d2b52e10b3cd4030ce0f80f21fff64
lija_lyfe <lija6556@list.ru>
2023-06-02 20:28:36 +0500

    add def test_get_last_values

--- test_utils.py
+++ test_utils.py
@@ -8,7 +8,7 @@ def test_get_data():
     assert isinstance(data, list)
 
 
-def test_get_filtered_data():
+def test_get_filtered_data(test_data):
     assert get_filtered_data(test_data[:2]) == [{
         "date": "2019-07-03T18:35:29.512364",
         "description": "Перевод организации",
@@ -20,8 +20,13 @@ def test_get_filtered_data():
                 "code": "USD"
             }
         },
-        "state": "EXECUTED"
+        "state": "EXECUTED",
         "to": "Счет 35383033474447895560"
     }]
     assert get_filtered_data(test_data[:2], filter_empty_from=True) == []
+
+
+def test_get_last_values(test_data):
+    data = get_last_values(test_data, 4)
+    assert [x["date"] for x in data] == ["2019-08-26T10:50:58.294041", "2019-07-03T18:35:29.512364", "2019-04-04T23:20:05.206878", "2019-03-23T01:09:46.296404"]
 
